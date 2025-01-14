40e6cfc2808b4c786482551b7f4e3a3ca4383612
Gunda Karthik <gundakarthik644@gmail.com>
2025-01-14 21:50:45 +0530

    Designed Landing pages of project

---
+++ url-shortener-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LandingPage from "./components/LandingPage.jsx";
+import AboutPage from "./components/AboutPage.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+      <>
+          <Router>
+              <Navbar/>
+              <Routes>
+                  <Route path="/" element={<LandingPage/>}/>
+                  <Route path="/about" element={<AboutPage/>}/>
+              </Routes>
+              <Footer/>
+          </Router>
+      </>
+  )
+}
+
+export default App
